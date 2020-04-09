6c4ee671be22535f22daf7650e0aed335a86b928
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-09 21:21:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@ def turn_count(board)
   turn_count = 0
   board.each do |position|
     turn = "X"
-    if "#{position}" == turn
-      turn = "O"
+    if "#{position}" == "X" || "#{positon}" == "O"
       turn_count += 1
     end
+  end
   return turn_count
 end
