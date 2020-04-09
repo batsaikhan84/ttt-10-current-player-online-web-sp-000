28fcc376ae6fa1bee16125b8e653395bc0f61fe2
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-09 21:14:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,7 @@
 def turn_count(board)
-  
-  
-  
-  board.each{|index| }
-  
   turn_count = 0
-  
-  turn_count += 1
+  turn = "X"
+  board.each{|index| == ""}
+    turn_count += 1
   return turn_count
 end
